c87663a3b350bc0f3622d38969f3d66677b66e3a
skywalkerzhang <1677858722@qq.com>
2023-09-05 23:21:41 +0800

    update self infomation

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,7 @@
-import { Show } from "solid-js";
-
 export default function Card(props) {
+    const extraClass = props.class ? " " + props.class : "";
     return (
-        <div class="rounded-xl bg-surface0 p-8 overflow-hidden shadow-md">
+        <div class={"rounded-xl bg-surface0 p-4 m-4 overflow-hidden shadow-md" + extraClass}>
             {props.children}
         </div>
     );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,55 +6,158 @@ import { faPhone, faEnvelope } from "@fortawesome/free-solid-svg-icons"
 
 export default function Home() {
     return (
-        <div class="mx-auto columns-2 h-full my-4">
-            <Card>
-                <div class="flex">
-                    <img src={ProfileImage} alt="profile"
-                        class="flex justify-start m-0 p-0 w-52 h-64 rounded-md" />
-                    <div class="mx-8 w-full">
-                        <h1 class="text-left text-2xl flex-row">
-                            Name: <span class="text-yellow">Yue Zhang</span>
-                        </h1>
-                        <div class="text-lg">Self Introduction</div>
-                        <p class="text-pink">
-                            blabla balblabla  blablablabla dafjlasdkjflksajfkl <br />
-                            and blablablablka <br />
-                            and blabla
-                        </p>
-                        <hr class="w-full mt-2" />
-                        <div class="mt-2">
-                            Contact Me
+        <div class="mx-auto flex flex-wrap h-full my-4">
+            <Card class="flex flex-wrap flex-grow">
+                <img src={ProfileImage} alt="profile"
+                    class="justify-start md:m-0 p-0 ml-16 mb-4 md:w-52 md:h-64 w-24 h-32 rounded-md" />
+                <div class="max-w-md ml-4">
+                    <h1 class="text-2xl">
+                        Name: <span class="text-yellow">Yue Zhang</span>
+                    </h1>
+                    <div class="text-lg">Self Introduction</div>
+                    <p class="text-pink mt-2">
+                        Hi I am Yue Zhang(Lunar) -  a CS PhD student at UTD, focusing on NLP and related fields.
+                    </p>
+                    <hr class="mt-2" />
+                    <div class="mt-12 text-lg">
+                        Contact Me
+                    </div>
+                    <div class="flex">
+                        <Fa icon={faEnvelope} scale={0.9} translateY={0.3} />
+                        <span class="text-green ml-2">
+                            <a class="hover:text-blue" href="mailto:skywalkerzhang19@163.com">
+                                skywalkerzhang19@163.com</a>
+                        </span>
+                    </div>
+                    <ul class="mt-2 flex">
+                        <li class="hover:text-blue hover:cursor-pointer mr-4">
+                            <a href="https://github.com/skywalkerzhang/profile/" target="_blank"     >
+                                <Fa icon={faGithub} />
+                            </a>
+                        </li>
+                        <li class="hover:text-blue hover:cursor-pointer mr-4">
+                            <a href="https://www.linkedin.com/in/yue-zhang-b44635287/" target="_blank">
+                                <Fa icon={faLinkedin} />
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </Card>
+            <Card class="flex-row flex-grow">
+                <div class="text-3xl">
+                    EDUCATION BACKGROUND
+                </div>
+                <hr class="mt-2" />
+                <div class="mt-4 flex-row">
+                    <div class="flex justify-between">
+                        <div class="font-bold">
+                            PhD student at University of Texas at Dallas
                         </div>
-                        <div class="flex"><Fa icon={faPhone} scale={0.9} translateY={0.3} />
-                            <span class="text-green ml-2">1111111111</span>
+                        <div>starting on 08/2023</div>
+                    </div>
+                    <div class="flex justify-between mt-2">
+                        <div class="font-bold">
+                            University of Manchester, UK
                         </div>
-                        <div class="flex"><Fa icon={faEnvelope} scale={0.9} translateY={0.3} />
-                            <span class="text-green ml-2"><a class="hover:text-blue" href="mailto:1111@2222.com">11111@222.com</a></span>
+                        <div>09/2020-09/2021</div>
+                    </div>
+                    <div>MSc in Advanced Computer Science | Distinction</div>
+                    <div class="flex justify-between mt-2">
+                        <div class="font-bold">
+                            Zhejiang Normal University, China
                         </div>
-                        <ul class="flex mt-4">
-                            <li class="hover:text-blue hover:cursor-pointer mr-4">
-                                <a href="https://github.com/skywalkerzhang" target="_blank"     >
-                                    <Fa icon={faGithub} />
-                                </a>
-                            </li>
-                            <li class="hover:text-blue hover:cursor-pointer mr-4">
-                                <a href="https://www.baidu.com" target="_blank">
-                                    <Fa icon={faLinkedin} />
-                                </a>
-                            </li>
-                        </ul>
+                        <div>09/2016-06/2020</div>
+                    </div>
+                    <div class="">
+                        B.E. in Computer Science and Technology | Overall GPA: 92/ 100 (Ranked: 1/34)
                     </div>
                 </div>
             </Card>
-            <Card>
+            <Card class="flex-row flex-grow">
                 <div class="text-3xl">
-                    Education Summary
+                    RESEARCH EXPERIENCES
                 </div>
-                <hr class="w-full mt-1" />
-                <ul class="mt-4">
-                    <li>TBD1</li>
-                    <li>TBD2</li>
-                </ul>
+                <hr class="mt-2" />
+                <div class="mt-4 flex-row">
+                    <div class="flex justify-between">
+                        <div class="font-bold">
+                            Research Assistant, Hong Kong University of Science and Technology(Guangzhou)
+                        </div>
+                        <div>04/2022-03/2023</div>
+                    </div>
+                    <div class="list-item ml-4">
+                        Conducted research on multi-modal documentation and OCR post-correction model;
+                    </div>
+                    <div class="list-item ml-4">
+                        Participated in the competition of VQAonBD(top3) and SVRD(top3), and used Donut(an OCR-free end2end model) to build a pipeline for document information extraction and document question answering;
+                    </div>
+                    <div class="list-item ml-4">
+                        Built a pipeline to process RTF(Rich Text Format) documents. The final F1 value on the SROIE dataset was 93.8590, and the accuracy of each entity increased from 68.4438 to 71.6858;
+                    </div>
+                    <div class="list-item ml-4">
+                        Built a pipeline on DocVQA using layoutlmv2; achieved 0.765 on this dataset, ranking 13 out of 45 on the ICDAR website.
+                    </div>
+                    <div class="flex justify-between mt-2">
+                        <div class="font-bold">
+                            Graduation Project--ECG (Electrocardiograph) QRS Feature Disease Recognition
+                        </div>
+                        <div>01/2021-09/2021</div>
+                    </div>
+                    <div class="list-item ml-4">
+                        Extracted QRS features using three methods of Pan, Lourenco, and Kalidas from PhysioNet, and completed two comparative models;
+                    </div>
+                    <div class="list-item ml-4">
+                        Entered features into Adaptive Lead Weighted ResNet with an accuracy of F1 accuracy, increasing the recall rate and accuracy by 1% for most diseases and by roughly 7% for other diseases.
+                    </div>
+                </div>
+            </Card>
+
+            <Card class="flex-row flex-grow">
+                <div class="text-3xl">
+                    AWARD and HONOURS
+                </div>
+                <hr class="mt-2" />
+                <div class="text-xl">
+                    Scholarships
+                </div>
+                <div class="flex justify-between">
+                    <div>
+                        President’s Special Award
+                    </div>
+                    <div>
+                        2020
+                    </div>
+                </div>
+                <div class="flex justify-between">
+                    <div>
+                        Second-class Scholarship of Zhejiang Normal University
+                    </div>
+                    <div>
+                        2019
+                    </div>
+                </div>
+                <div class="flex justify-between">
+                    <div>
+                        Government Scholarship Awarded by Education Department of Zhejiang Province
+                    </div>
+                    <div>
+                        2018
+                    </div>
+                </div>
+                <div class="flex justify-between">
+                    <div>
+                        Second-class Scholarship of Zhejiang Normal University
+                    </div>
+                    <div>
+                        2017
+                    </div>
+                </div>
+            </Card>
+            <Card class="flex-row flex-grow w-5/6">
+                <div class="text-3xl">
+                    PAPERS
+                </div>
+                <hr class="mt-2" />
             </Card>
         </div>
     )
