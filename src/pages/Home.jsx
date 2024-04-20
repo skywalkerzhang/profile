888fcfbae89bd585d816160ec44eb2d118ce9f6b
Skywalker <37722763+skywalkerzhang@users.noreply.github.com>
2024-04-20 18:25:00 -0500

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -16,7 +16,7 @@ export default function Home() {
                     </h1>
                     <div class="text-lg">Self Introduction</div>
                     <p class="text-pink mt-2">
-                        Hi I am Yue Zhang (Lunar) -  a CS PhD student at UTD, focusing on NLP and related fields.
+                        Hi I am Yue Zhang (Lunar) -  a CS PhD student at UTD, focusing on Vision and Language and related fields.
                     </p>
                     <hr class="mt-2" />
                     <div class="mt-12 text-lg">
